8b5f59ea2bbdb8c7152e5701b500506e5d4bfbbb
hollarteju <sirteju2022@gmail.com>
2024-01-14 02:22:31 +0100

    land page desktop and homepage toggle

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("logo").addEventListener("click", function(){
+    document.getElementById("home-headline").classList.add("landpage-headline-container");
+    document.getElementById("demo-btn").classList.add("demo-btn");
+    document.getElementById("login-btn").classList.add("login-btn-disable");
+
+})
