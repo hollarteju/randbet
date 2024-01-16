2f7350e2240ba8e61586e82bb2e2c692f5b10f8e
hollarteju <sirteju2022@gmail.com>
2024-01-16 04:36:47 +0100

    rsgister page done!

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ document.getElementById("menu-btn").addEventListener("click", function(){
     document.getElementById("login-btn").classList.toggle("nav-active");
 
 })
+
+document.getElementById("register-btn").addEventListener("click", function(){
+    document.getElementById("register-pg").classList.add("register-activate")
+})
+
+document.getElementById("canceal-btn").addEventListener("click", function(){
+    document.getElementById("register-pg").classList.remove("register-activate");
+    document.getElementById("login-btn").classList.remove("nav-active");
+})
