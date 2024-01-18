42de8f7fbc104ccb8c02986fc3aab8985a7dc07e
hollarteju <sirteju2022@gmail.com>
2024-01-18 06:40:42 +0100

    login page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.getElementById("menu-btn").addEventListener("click", function(){
     // document.getElementById("home-headline").classList.add("landpage-headline-container");
-    // document.getElementById("demo-btn").classList.add("demo-btn");
-    document.getElementById("login-btn").classList.toggle("nav-active");
+    // document.getElementById("demo").classList.add("demo");
+    document.getElementById("login").classList.toggle("nav-active");
 
 })
 
@@ -11,5 +11,14 @@ document.getElementById("register-btn").addEventListener("click", function(){
 
 document.getElementById("canceal-btn").addEventListener("click", function(){
     document.getElementById("register-pg").classList.remove("register-activate");
-    document.getElementById("login-btn").classList.remove("nav-active");
+    document.getElementById("login").classList.remove("nav-active");
+})
+
+document.getElementById("login-btn").addEventListener("click", function(){
+    document.getElementById("login-pg").classList.add("register-activate")
+})
+
+document.getElementById("login-x").addEventListener("click", function(){
+    document.getElementById("login-pg").classList.remove("register-activate");
+    document.getElementById("login").classList.remove("nav-active");
 })
